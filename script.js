9df3260a9e259d13027856f74b9218c1656ac0d8
Biju George <66988611+Biju71@users.noreply.github.com>
2025-07-11 14:17:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function greetUser() {
   alert("Hello Biju! Welcome to my portfolio site.");
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggle-theme");
+
+  toggleButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+  });
+});
