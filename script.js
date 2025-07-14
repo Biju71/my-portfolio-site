f012da92ca0fd0d20c4bfa90129cc1b0cc3be527
Biju George <66988611+Biju71@users.noreply.github.com>
2025-07-13 20:42:47 -0500

    Add files via upload
    Added time clock

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.toggle("dark-mode");
   });
 });
+function updateClock() {
+  const now = new Date();
+  const timeString = now.toLocaleTimeString();
+  document.getElementById("live-clock").textContent = timeString;
+}
+
+// Start the clock
+setInterval(updateClock, 1000);
+updateClock(); // call it once immediately
