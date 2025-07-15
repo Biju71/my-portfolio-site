217ec35cb57e0abec70484186e572826108b148c
Biju George <66988611+bijgeorge@users.noreply.github.com>
2025-07-15 18:49:24 -0500

    Add files via upload
    Added folder

---
+++ ui-sandbox/script.js
@@ -0,0 +1,5 @@
+function changeContent() {
+  const title = document.getElementById("main-title");
+  title.textContent = "You clicked the button!";
+  document.body.style.backgroundColor = "#f0f8ff";
+}
