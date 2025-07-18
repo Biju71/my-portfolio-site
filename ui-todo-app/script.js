0cee5ea0fce009b1ea5ffcd2a24617e6fe42894d
Biju George <66988611+bijgeorge@users.noreply.github.com>
2025-07-17 20:58:36 -0500

    Add files via upload
    PWA

--- ui-todo-app/script.js
+++ ui-todo-app/script.js
@@ -81,3 +81,11 @@ function loadTasks() {
   const tasks = getTasksFromStorage();
   renderTasks(tasks);
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(reg => console.log("Service Worker registered!", reg))
+      .catch(err => console.error("Service Worker registration failed:", err));
+  });
+}
